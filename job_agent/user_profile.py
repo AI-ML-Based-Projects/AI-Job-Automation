ff16f68d731b659b85c6713c6373cb35eb7dd5a2
akhi055 <akhilanampally05@gmail.com>
2026-02-12 23:50:55 +0530

    Initial commit

---
+++ job_agent/user_profile.py
@@ -0,0 +1,20 @@
+class UserProfile:
+    NAME = "Nampally Akhila"
+    EMAIL = "akhiakhila049@gmail.com"
+    PHONE = "7095101397"
+    LINKEDIN = "nampally-akhila-200274263"
+    GITHUB = "akhi055"
+    LOCATION = "Hyderabad, India" # Inferred or default
+    EXPERIENCE_YEARS = 0 # Fresher
+    SKILLS = [
+        "Python", "Machine Learning", "Scikit-learn", "Pandas", "Numpy", 
+        "Deep Learning", "MySQL", "Git", "GitHub", "Tableau", "NLP", 
+        "TensorFlow", "Keras" # Inferred from projects
+    ]
+    EDUCATION = {
+        "Degree": "B.Tech (Artificial Intelligence and Machine Learning)",
+        "College": "Vignan Institute of Management and Technology For Women",
+        "Year": "2024",
+        "CGPA": "8.0"
+    }
+    RESUME_PATH = "resume.pdf" # User needs to place this file
